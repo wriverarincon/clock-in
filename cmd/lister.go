839751ff0a06f689938d4c5e13bf9a4e266582f8
Wirdin Rivera <wrr201@hotmail.com>
2025-05-17 16:31:17 -0400

    WIP: Added output structure

--- cmd/lister.go
+++ cmd/lister.go
@@ -13,13 +13,42 @@ type HelpCommand struct {
 	Registry *commands.Registry
 }
 
-func (p *HelpCommand) Execute(args []string) {
-	r := p.Registry
-	writer := tabwriter.NewWriter(os.Stdout, 0, 4, 2, ' ', tabwriter.AlignRight)
-	fmt.Println("COMMANDS")
-	for k, v := range r.Commands {
-		shortDescription := v.MetaData.ShortDescription
-		fmt.Fprintln(writer, strings.Join([]string{k, shortDescription}, "\t")+"\t")
+func printCommands(commands map[string]commands.Command) {
+	writer := tabwriter.NewWriter(os.Stdout, 0, 4, 2, ' ', 0)
+	fmt.Println("CORE COMMANDS")
+	for k, v := range commands {
+		shortd := v.MetaData.ShortDescription
+		fmt.Fprintln(writer, "  "+strings.Join([]string{k, shortd}, "\t")+"\t")
 	}
 	writer.Flush()
+}
+
+func printSubCommands(subCommands [][]string) {
+	writer := tabwriter.NewWriter(os.Stdout, 0, 4, 2, ' ', 0)
+	fmt.Println("SUB COMMANDS")
+	for _, v := range subCommands {
+		fmt.Fprintln(writer, "  "+strings.Join([]string{v[0], v[1]}, "\t")+"\n")
+	}
+	writer.Flush()
+}
+
+func printFlags(flags [][]string) {
+	writer := tabwriter.NewWriter(os.Stdout, 0, 4, 2, ' ', 0)
+	fmt.Println("FLAGS")
+	for _, v := range flags {
+		fmt.Fprintln(writer, "  "+strings.Join([]string{v[0], v[1]}, "\t"))
+	}
+	writer.Flush()
+}
+
+func (p *HelpCommand) Execute(args []string) {
+	r := p.Registry
+	if len(args) >= 1 {
+		printSubCommands(r.Commands[args[0]].MetaData.SubCommands)
+		printFlags(r.Commands[args[0]].MetaData.Flags)
+
+	} else {
+		fmt.Println("USAGE\n  <app> <command> <subcommand> [flags]\n")
+		printCommands(r.Commands)
+	}
 }
