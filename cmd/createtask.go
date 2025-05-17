052a3eb3a2415d32e58d5fabab15341f89953d5b
Wirdin Rivera <wrr201@hotmail.com>
2025-05-17 12:18:23 -0400

    WIP: Added flags and subcommand support

--- cmd/createtask.go
+++ cmd/createtask.go
@@ -1,10 +1,39 @@
 package cmd
 
-type CreateTask struct{}
+import (
+	"flag"
+	"fmt"
+	"os"
 
-func (ct *CreateTask) Execute(args []string) {
-	var argsMapper map[string]string
-	for i := 0; i < len(args); i += 2 {
-		argsMapper[arg][]
+	"github.com/wriverarincon/clock-in/internal/repository/commands"
+)
+
+type TaskCommand struct {
+	Registry *commands.Registry
+}
+
+var (
+	title string
+	body  string
+	from  string
+	until string
+)
+
+func (t *TaskCommand) createTask(args []string) {
+	fs := flag.NewFlagSet("create", flag.ContinueOnError)
+	fs.SetOutput(os.Stdout)
+	fs.StringVar(&title, "title", "", "task title")
+	fs.StringVar(&body, "body", "", "task body")
+
+	if err := fs.Parse(args); err != nil {
+		fmt.Println("Error parsing flags:", err)
+		return
 	}
+}
+
+func (t *TaskCommand) Execute(args []string) {
+	t.Registry.AddSubCommand(args[1], "create", t.createTask)
+
+	t.Registry.ExecuteSubCommand(args)
+	fmt.Printf("Task %v created\n", title)
 }
