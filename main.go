58133cb8070be2705edbae6942bf59cbf2418ef6
Wirdin Rivera <wrr201@hotmail.com>
2025-05-17 16:30:00 -0400

    Removed redundant fields and changed its name

--- main.go
+++ main.go
@@ -17,19 +17,21 @@ func main() {
 		ShortDescription: "Initializes the app",
 		LongDescription: `The "init" command initializes the app, retrieving the tasks completed the previos day,
 						  along with basic analytics facts.`,
-		AvailableOptions: [][]string{
+		Flags: [][]string{
 			{"-Q, --quiet", "Initializes the app in quiet mode, not returning any output."},
 		},
 	}
 	helpMetaData := commands.MetaData{
 		ShortDescription: "Prints this message",
 		LongDescription:  "Prints this message along with all the available commands.",
-		AvailableOptions: [][]string{},
 	}
 	createTaskMetaData := commands.MetaData{
 		ShortDescription: "Create task",
 		LongDescription:  "Creates and stores a task for the day and [OPTIONS] specified",
-		AvailableOptions: [][]string{
+		SubCommands: [][]string{
+			{"create", "create a new task"},
+		},
+		Flags: [][]string{
 			{"-t, --title", "Title of the task"},
 			{"-b, --body", "Body of the task"},
 			{"--start-day", "Determines the start day of the task, defaults to today"},
