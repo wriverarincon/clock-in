1804b89ea190a876caeba0976ed7648b673edd94
Wirdin Rivera <wrr201@hotmail.com>
2025-05-21 11:34:25 -0400

    Refactored the main function, removing unnecesary code

--- main.go
+++ main.go
@@ -1,20 +1,16 @@
 package main
 
 import (
-	"github.com/wriverarincon/clock-in/cmd"
-	"github.com/wriverarincon/clock-in/internal/repository/commands"
+	"os"
+
+	"github.com/wriverarincon/clock-in/cmd/task"
+	"github.com/wriverarincon/command"
 )
 
 func main() {
 	// Create the registry
-	registry := commands.NewRegistry()
+	registry := command.NewRegistry()
 
-	init := &cmd.InitCommand{}
-	help := &cmd.HelpCommand{Registry: registry}
-	task := &cmd.TaskCommand{Registry: registry}
-
-	registry.NewCommand("init", init.Init, cmd.InitMetaData)
-	registry.NewCommand("help", help.Init, cmd.HelpMetaData)
-	registry.NewCommand("task", task.Init, cmd.TaskMetaData)
-	registry.Execute()
+	registry.New(nil, task.New(), task.Setup(registry))
+	registry.Execute(os.Args[1:])
 }
