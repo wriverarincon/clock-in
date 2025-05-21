68a3a4aba04136b7eeab8606852542b98e9b3e5d
Wirdin Rivera <wrr201@hotmail.com>
2025-05-21 09:07:08 -0400

    Moved `create`'s setup to its source file

--- cmd/task/create.go
+++ cmd/task/create.go
@@ -5,6 +5,28 @@ import "github.com/wriverarincon/command"
 type taskCreate struct{}
 
 func (c taskCreate) Execute(args []string) error {
+	var (
+		title string
+		body  string
+		from  string
+		until string
+	)
+
+	fs := flag.NewFlagSet("create", flag.ContinueOnError)
+	fs.SetOutput(os.Stdout)
+	fs.StringVar(&title, "title", "", "task title")
+	fs.StringVar(&title, "t", "", "task title (shorthand)")
+	fs.StringVar(&body, "body", "", "task body")
+	fs.StringVar(&body, "b", "", "task body (shorthand)")
+	fs.StringVar(&from, "from", "", "task start time")
+	fs.StringVar(&from, "f", "", "task start time (shorthand)")
+	fs.StringVar(&until, "until", "", "task end time")
+	fs.StringVar(&until, "u", "", "task end time (shorthand)")
+
+	if err := fs.Parse(args); err != nil {
+		fmt.Println("Error parsing flags:", err)
+		return err
+	}
 	return nil
 }
 
--- cmd/task/task.go
+++ cmd/task/task.go
@@ -1,9 +1,6 @@
 package task
 
 import (
-	"flag"
-	"fmt"
-	"os"
 	"time"
 
 	"github.com/wriverarincon/command"
@@ -70,30 +67,5 @@ func Setup(registry *command.Registry) {
 	}
 	for _, v := range subCommands {
 		registry.New(v.path, v.fn)
-	}
-}
-
-func (t *TaskCommand) createTask(args []string) {
-	var (
-		title string
-		body  string
-		from  string
-		until string
-	)
-
-	fs := flag.NewFlagSet("create", flag.ContinueOnError)
-	fs.SetOutput(os.Stdout)
-	fs.StringVar(&title, "title", "", "task title")
-	fs.StringVar(&title, "t", "", "task title (shorthand)")
-	fs.StringVar(&body, "body", "", "task body")
-	fs.StringVar(&body, "b", "", "task body (shorthand)")
-	fs.StringVar(&from, "from", "", "task start time")
-	fs.StringVar(&from, "f", "", "task start time (shorthand)")
-	fs.StringVar(&until, "until", "", "task end time")
-	fs.StringVar(&until, "u", "", "task end time (shorthand)")
-
-	if err := fs.Parse(args); err != nil {
-		fmt.Println("Error parsing flags:", err)
-		return
 	}
 }
