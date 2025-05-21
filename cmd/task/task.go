72a9d75ae22c9a47e5ca9b013c66f80dac5d5106
Wirdin Rivera <wrr201@hotmail.com>
2025-05-21 11:32:06 -0400

    Added missing command name

--- cmd/task/task.go
+++ cmd/task/task.go
@@ -19,6 +19,7 @@ var startFlag = command.NewFlag("start-day", "", "Start day of the task, default
 var endFlag = command.NewFlag("end-day", "", "End day of the task, defaults to today", time.Now().Format("2006-01-02"), false)
 
 var TaskMetaData = command.MetaData{
+	Name:             "task",
 	ShortDescription: "Create task",
 	LongDescription:  "Creates and stores a task for the day and [OPTIONS] specified",
 	Flags:            []command.Flag{titleFlag, bodyFlag, startFlag, endFlag},
